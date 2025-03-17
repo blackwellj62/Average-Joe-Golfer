9eba8af3f9f54425e139c578dae2d1bf4e8283cc
Joey Blackwell <blackwellj6241@gmail.com>
2025-03-17 11:04:24 -0500

    added authentication and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Login } from './Components/Auth/login.jsx'
+import { Register } from './Components/Auth/Register.jsx'
+import { Authorized } from './Views/Authorized.jsx'
+import { ApplicationViews } from './Views/ApplicationViews.jsx'
 
 export const App = () => {
-
+    return(
+        <Routes>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="*" element={
+        <Authorized>
+          <ApplicationViews/>
+        </Authorized>
+      }/>
+    </Routes>
+    )
 }
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css"
+
+export const Home = () => {
+    return(
+        <div className="welcome-container">
+            <h1>Welcome to Average Joe Golfer
+            where the swings are wild, the putts are questionable, 
+            and the only thing lower than our scores is our self-esteem! 
+            Grab your clubs... and maybe a stress ball⛳</h1>
+        </div>
+    )
+}
