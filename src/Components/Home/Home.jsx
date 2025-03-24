86fb264d5da44d95caef18ed13c7b06e0b41d777
Joey Blackwell <blackwellj6241@gmail.com>
2025-03-24 13:30:23 -0500

    added enter button to home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,21 @@
+import { useNavigate } from "react-router-dom"
 import "./Home.css"
 
 export const Home = () => {
+    const navigate = useNavigate()
+
     return(
+        <>
         <div className="welcome-container">
             <h1>Welcome to Average Joe Golfer</h1>
             <p>where the swings are wild, the putts are questionable, 
             and the only thing lower than our scores is our self-esteem! 
             Grab your clubs... and maybe a stress ball⛳</p>
         </div>
+        <div className="button-container">
+            <button className="btn btn-primary" onClick={()=>{navigate("/posts")}}>Enter</button>
+        </div>
+        </>
+        
     )
 }
